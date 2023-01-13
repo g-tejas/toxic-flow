1d7734c4399bb82cd1df4af3aa02b67122c27f8f
Tejas G <ttejasgarrepally@gmail.com>
2023-01-13 21:18:58 +0800

    separate historical subcommand from monte carlo and created submodule

---
+++ src/historical/mod.rs
@@ -0,0 +1,73 @@
+use serde::{Deserialize, Serialize};
+use chrono::{Duration, NaiveDate};
+use clap::Args;
+use std::mem;
+pub mod parser;
+
+#[derive(Debug, Deserialize)]
+pub struct InputRow {
+    pub id: String,
+    pub price: f64,
+    pub qty: f64,
+    pub quote_qty: f64,
+    pub time: i64,
+    pub is_buyer_maker: bool,
+}
+
+// start_time needs to be signed because of algo
+#[derive(Debug, Serialize)]
+pub struct OutputRow {
+    pub bucket_no: i64,
+    pub agg_buy: f64,
+    pub agg_sell: f64,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub order_imbalance: f64,
+}
+
+pub struct DateRange(pub NaiveDate, pub NaiveDate);
+
+impl Iterator for DateRange {
+    type Item = NaiveDate;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 <= self.1 {
+            let next = self.0 + Duration::days(1);
+            Some(mem::replace(&mut self.0, next))
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Args, Debug)]
+pub struct Options {
+    /// Sample of volume buckets used in the estimation
+    #[arg(short, default_value_t = 50)]
+    pub n: u32,
+    /// Volume bucket size
+    #[arg(short, default_value_t = 6894.)]
+    pub volume_bucket_size: f64,
+    /// Enter the start date in the format YYYY-MM-DD. For example, 2020-12-31.
+    #[arg(short, default_value_t = String::from("2020-01-01"))]
+    pub start_date: String,
+    /// Enter the end date in the format YYYY-MM-DD. For example, 2020-12-31.
+    #[arg(short, default_value_t = String::from("2022-12-20"))]
+    pub end_date: String,
+    /// Path to input directory with all the CSV files.
+    #[arg(short)]
+    pub input_dir: String,
+    /// Path to output file
+    #[arg(short, default_value_t = String::from("output.csv"))]
+    pub output_file: String,
+}
+
+impl Options {
+    /// Returns the a struct, DateRange, with a custom iterator
+    pub fn get_date_range(&self) -> DateRange {
+        let start_date: NaiveDate =
+            NaiveDate::parse_from_str(self.start_date.as_str(), "%Y-%m-%d").unwrap();
+        let end_date: NaiveDate =
+            NaiveDate::parse_from_str(self.end_date.as_str(), "%Y-%m-%d").unwrap();
+        DateRange(start_date, end_date)
+    }
+}
---
+++ src/historical/parser.rs
@@ -0,0 +1,125 @@
+use indicatif::{ProgressState, ProgressBar, ProgressStyle};
+use csv::StringRecord;
+use std::{error::Error, fmt::Write};
+use super::Options;
+
+pub fn get_buckets(opt: &Options) -> Result<(), Box<dyn Error>> {
+    
+    let date_range = opt.get_date_range();
+    let mut rows: Vec<super::OutputRow> = Vec::new();
+
+    // These parameter's must persist across all csv files so
+    // put them outside all for loops
+    let mut curr_bucket: i64 = 1;
+    let mut curr_vol: f64 = 0.;
+    let mut curr_buy_vol: f64 = 0.;
+    let mut curr_sell_vol: f64 = 0.;
+    let mut start_time: i64 = -1;
+    let mut last_time: i64 = 0;
+
+    let pb = ProgressBar::new((date_range.1 - date_range.0).num_days() as u64);
+    pb.set_style(
+        ProgressStyle::with_template(
+            "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos:>7}/{len:7} {msg}",
+        )
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
+            write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
+        })
+        .progress_chars("#>-"),
+    );
+
+    for dt in date_range {
+        let mut rdr = csv::Reader::from_path(format!("{}/BTCUSDT-trades-{}.csv", opt.input_dir, dt))?;
+
+        // Some of the binance csv files have headers, some do not, we need to take care of those that don't
+        // Since header rows do not appear in the iterator, we have to assert beforehand
+        let headers = rdr.headers().unwrap();
+        let real_headers: StringRecord = StringRecord::from(vec![
+            "id",
+            "price",
+            "qty",
+            "quote_qty",
+            "time",
+            "is_buyer_maker",
+        ]);
+        if headers == &real_headers {
+        } else {
+            rdr.set_headers(real_headers);
+        }
+
+        for row in rdr.deserialize() {
+            let mut row: super::InputRow = row?;
+
+            last_time = row.time;
+
+            if start_time == -1 {
+                start_time = row.time;
+            }
+            while row.qty + curr_vol > opt.volume_bucket_size {
+                // how much u need to fill up basically
+                let delta: f64 = opt.volume_bucket_size - curr_vol;
+
+                if row.is_buyer_maker {
+                    curr_buy_vol += delta;
+                } else {
+                    curr_sell_vol += delta;
+                }
+
+                curr_bucket += 1;
+                let new_output: super::OutputRow = super::OutputRow {
+                    bucket_no: curr_bucket,
+                    agg_buy: curr_buy_vol,
+                    agg_sell: curr_sell_vol,
+                    start_time,
+                    end_time: row.time,
+                    order_imbalance: (curr_buy_vol - curr_sell_vol).abs(),
+                };
+                rows.push(new_output);
+                start_time = row.time;
+                row.qty -= delta;
+                curr_vol = 0.;
+                curr_buy_vol = 0.;
+                curr_sell_vol = 0.;
+            }
+            curr_vol += row.qty;
+
+            if row.is_buyer_maker {
+                curr_buy_vol += row.qty;
+            } else {
+                curr_sell_vol += row.qty;
+            }
+        }
+
+        // Writing out
+        let mut wtr = csv::Writer::from_path(opt.output_file.clone())?;
+        for row in &rows {
+            wtr.serialize(row)?;
+        }
+        wtr.flush()?;
+        pb.inc(1);
+    }
+
+    // Write out one last time, for the remainder volume
+    let mut wtr = csv::Writer::from_path(opt.output_file.clone())?;
+
+    curr_bucket += 1;
+    let left_over: super::OutputRow = super::OutputRow {
+        bucket_no: curr_bucket,
+        agg_buy: curr_buy_vol,
+        agg_sell: curr_sell_vol,
+        start_time,
+        end_time: last_time,
+        order_imbalance: (curr_buy_vol - curr_sell_vol).abs(),
+    };
+    rows.push(left_over);
+
+    for row in rows {
+        //println!("{:?}", row);
+        wtr.serialize(row)?;
+    }
+    wtr.flush()?;
+    pb.finish_with_message("processed!");
+
+    Ok(())
+}
