a7d103c43ca414b86725a5e76fc6040a6ea097c7
Tejas G <ttejasgarrepally@gmail.com>
2023-01-13 21:40:30 +0800

    changed shit

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,13 @@
 /// For optimization, refer to documentation above, but for now it runs pretty fast.
 use clap::Parser;
 use tflow::*;
-use tflow::historical::parser::get_buckets;
-use std::time::Instant;
-
+use tflow::historical::parser::run;
 
 fn main() {
     let cli = Cli::parse();
 
-    let now = Instant::now(); // For timing
-
      match &cli.command {
-        Commands::Historical(opts) => get_buckets(opts).expect("Failed to process csv."),
+        Commands::Historical(opts) => run(opts).expect("Failed to process csv."),
         Commands::MonteCarlo => println!("Hello world!"),
      } 
-
-    println!("Processing csv took: {} seconds", now.elapsed().as_secs());
 }
