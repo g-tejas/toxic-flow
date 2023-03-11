cac4ff2b9b380e6403221267ab6ba79cfeff77a1
Tejas G <ttejasgarrepally@gmail.com>
2023-03-11 10:18:17 +0800

    aloysius!

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use chrono::{DateTime, Utc};
 use clap::{Parser, Subcommand};
 use serde::Serialize;
 use tracing::*;
-
+use std::collections::VecDeque;
 pub mod historical;
 pub mod montecarlo;
 pub mod live;
@@ -44,6 +44,71 @@ pub struct Buckets {
 	pub current_sell: f64,
 	pub threshold: f64,
 	pub current_id: i64,
+}
+#[derive(Debug, Serialize)]
+pub struct MovingBucket {
+	pub current_buy: f64,
+	pub current_sell: f64,
+	pub current_imbalance: f64,
+	pub threshold: f64,
+	pub dq: VecDeque<f64>,
+}
+impl MovingBucket {
+	pub fn new(threshold: f64) -> Self {
+		Self {
+			current_buy: 0.,
+			current_sell: 0.,
+			current_imbalance: 0.,
+			threshold,
+			dq: VecDeque::new()
+		}
+	}
+
+	#[instrument]
+	pub fn process_trade(&mut self, trade: MarketEvent<PublicTrade>) {
+
+		match trade.kind.side {
+			Side::Buy => {
+				self.dq.push_back(trade.kind.amount);
+				self.current_buy += trade.kind.amount;
+			}
+			Side::Sell => {
+				self.dq.push_back(-trade.kind.amount);
+				self.current_sell += trade.kind.amount;
+			}
+		}
+		loop {
+			let excess = self.current_buy + self.current_sell - self.threshold;
+			if excess <= 0. {
+				break;
+			}
+			let front_most = self.dq.pop_front();
+			match front_most {
+				None => {break;}
+				Some(curval) => {
+					if curval < 0. {
+						self.current_sell -= curval.abs();
+					} else {
+						self.current_buy -= curval.abs();
+					}
+					if curval.abs() < excess {
+						continue;
+					} 
+					let to_add_back = curval.abs() - excess;
+					if curval < 0. {
+						self.dq.push_front(-to_add_back);
+						self.current_sell += to_add_back;
+					} else {
+						self.dq.push_front(to_add_back);
+						self.current_buy += to_add_back;
+					}
+				}
+			}
+		}
+
+		self.current_imbalance = (self.current_buy - self.current_sell).abs();
+		println!("The current imbalance is {:.6}", self.current_buy-self.current_sell);
+	}
 }
 
 impl Buckets {
@@ -102,3 +167,4 @@ impl Buckets {
 		}
 	}
 }
+
--- src/live/server.rs
+++ src/live/server.rs
@@ -1,20 +1,20 @@
 use barter_data::{
-    exchange::binance::futures::BinanceFuturesUsd,
-    streams::Streams,
-    subscription::trade::PublicTrades,
+	exchange::binance::futures::BinanceFuturesUsd,
+	streams::Streams,
+	subscription::trade::PublicTrades,
 };
 use barter_integration::model::InstrumentKind;
 use futures::StreamExt;
 use tracing::*;
 
-use crate::Buckets;
+use crate::MovingBucket;
 
 use super::Options;
 
 #[instrument]
 #[tokio::main]
 pub async fn run(opts: &Options) -> Result<(), Box<dyn std::error::Error>> {
-	let mut vpin = Buckets::new(10.);
+	let mut vpin = MovingBucket::new(10.);
 
 	let streams = Streams::<PublicTrades>::builder()
 		.subscribe([(BinanceFuturesUsd::default(), "btc", "usdt", InstrumentKind::FuturePerpetual, PublicTrades)])
@@ -23,7 +23,7 @@ pub async fn run(opts: &Options) -> Result<(), Box<dyn std::error::Error>> {
 		.unwrap();
 
 	let mut joined_stream = streams.join_map().await;
-	while let Some((exchange, trade)) = joined_stream.next().await {
+	while let Some((_exchange, trade)) = joined_stream.next().await {
 		vpin.process_trade(trade);
 	}
 
